0e4e120e5b086bbebdf1b5e183a8cee6777c449b
Bruno Pitteli Gonçalves <scorninpc@gmail.com>
2023-11-26 22:56:29 -0300

    enable set command on console

--- src/main/java/me/refracdevelopment/simpletags/commands/command/SetCommand.java
+++ src/main/java/me/refracdevelopment/simpletags/commands/command/SetCommand.java
@@ -12,6 +12,7 @@ import me.refracdevelopment.simpletags.player.data.Tag;
 import me.refracdevelopment.simpletags.manager.configuration.LocaleManager;
 import me.refracdevelopment.simpletags.player.data.ProfileData;
 import me.refracdevelopment.simpletags.utilities.Permissions;
+import me.refracdevelopment.simpletags.utilities.chat.Color;
 import me.refracdevelopment.simpletags.utilities.Tasks;
 import me.refracdevelopment.simpletags.utilities.Utilities;
 import me.refracdevelopment.simpletags.utilities.chat.Placeholders;
@@ -28,46 +29,51 @@ public class SetCommand extends RoseCommand {
     public void execute(CommandContext context, @Optional OfflinePlayer target, @Optional String configName) {
         final LocaleManager locale = this.rosePlugin.getManager(LocaleManager.class);
 
+
+        Player player = null;
+
         // Make sure the sender is a player.
-        if (!(context.getSender() instanceof Player)) {
-            locale.sendMessage(context.getSender(), "no-console");
-            return;
-        }
+        if ((context.getSender() instanceof Player)) {
+            
+            Player player = (Player) context.getSender();
 
-        Player player = (Player) context.getSender();
-
-        // Set a player tag
-        if (SimpleTags.getInstance().getTagManager().getCachedTag(configName) == null) {
-            locale.sendMessage(player, "invalid-tag", Placeholders.setPlaceholders(player));
-            return;
-        }
-
-        if (target == null) {
-            ProfileData profile = SimpleTags.getInstance().getProfileManager().getProfile(player.getUniqueId()).getData();
-            Tag tag = SimpleTags.getInstance().getTagManager().getCachedTag(configName);
-
-            StringPlaceholders placeholders = StringPlaceholders.builder()
-                    .addAll(Placeholders.setPlaceholders(context.getSender()))
-                    .add("player", player.getName())
-                    .add("tag-name", tag.getTagName())
-                    .add("tag-prefix", tag.getTagPrefix())
-                    .build();
-
-            if (!player.hasPermission("simpletags.tag." + configName)) {
-                locale.sendMessage(player, "tag-not-owned");
+            // Set a player tag
+            if (SimpleTags.getInstance().getTagManager().getCachedTag(configName) == null) {
+                locale.sendMessage(player, "invalid-tag", Placeholders.setPlaceholders(player));
                 return;
             }
 
-            profile.setTag(configName);
-            profile.setTagPrefix(tag.getTagPrefix());
-            Tasks.runAsync(rosePlugin, profile::save);
-            locale.sendMessage(player, "tag-updated", placeholders);
-            return;
+            if (target == null) {
+                ProfileData profile = SimpleTags.getInstance().getProfileManager().getProfile(player.getUniqueId()).getData();
+                Tag tag = SimpleTags.getInstance().getTagManager().getCachedTag(configName);
+
+                StringPlaceholders placeholders = StringPlaceholders.builder()
+                        .addAll(Placeholders.setPlaceholders(context.getSender()))
+                        .add("player", player.getName())
+                        .add("tag-name", tag.getTagName())
+                        .add("tag-prefix", tag.getTagPrefix())
+                        .build();
+
+                if (!player.hasPermission("simpletags.tag." + configName)) {
+                    locale.sendMessage(player, "tag-not-owned");
+                    return;
+                }
+
+                profile.setTag(configName);
+                profile.setTagPrefix(tag.getTagPrefix());
+                Tasks.runAsync(rosePlugin, profile::save);
+                locale.sendMessage(player, "tag-updated", placeholders);
+                return;
+            }
+
+            if (!player.hasPermission(Permissions.SET_OTHER_COMMAND)) {
+                locale.sendMessage(player, "no-permission");
+                return;
+            }
         }
 
-        if (!player.hasPermission(Permissions.SET_OTHER_COMMAND)) {
-            locale.sendMessage(player, "no-permission");
-            return;
+        if (target == null) {
+            Color.log("You need to specify a target"); // maybe translate?
         }
 
         if (target.isOnline()) {
@@ -84,7 +90,12 @@ public class SetCommand extends RoseCommand {
             profile.setTag(configName);
             profile.setTagPrefix(tag.getTagPrefix());
             Tasks.runAsync(rosePlugin, profile::save);
-            locale.sendMessage(player, "tag-set", placeholders);
+            if(player != null) {
+                locale.sendMessage(player, "tag-set", placeholders);
+            }
+            else {
+                Color.log("Tag updated"); // maybe translate?
+            }
             locale.sendMessage(profile.getPlayer(), "tag-updated", placeholders);
         } else {
             Tag tag = SimpleTags.getInstance().getTagManager().getCachedTag(configName);
@@ -99,7 +110,12 @@ public class SetCommand extends RoseCommand {
             Tasks.runAsync(rosePlugin, () -> {
                 Utilities.saveOfflinePlayer(target.getUniqueId(), target.getName(), tag.getTagName(), tag.getTagPrefix());
             });
-            locale.sendMessage(player, "tag-set", placeholders);
+            if(player != null) {
+                locale.sendMessage(player, "tag-set", placeholders);
+            }
+            else {
+                Color.log("Tag updated"); // maybe translate?
+            }
         }
     }
 
