8974c991d0c31b524cfdd1fb0d9c3bf395214157
Zach <zacharybaldwin904@gmail.com>
2024-05-17 22:07:46 -0500

    readd custom model data version check

--- src/main/java/me/refracdevelopment/simpletags/utilities/Tasks.java
+++ src/main/java/me/refracdevelopment/simpletags/utilities/Tasks.java
@@ -7,14 +7,11 @@ import java.util.concurrent.TimeUnit;
 
 public class Tasks {
 
-    private Tasks() {
-    }
-
     public static void runAsync(Runnable callable) {
         SimpleTags.getInstance().getPaperLib().scheduling().asyncScheduler().run(callable);
     }
 
     public static void runAsyncTimer(Runnable callable, long delay) {
-        SimpleTags.getInstance().getPaperLib().scheduling().asyncScheduler().runDelayed(callable, Duration.of(delay, TimeUnit.SECONDS.toChronoUnit()));
+        SimpleTags.getInstance().getPaperLib().scheduling().asyncScheduler().runDelayed(callable, Duration.of(delay, TimeUnit.MICROSECONDS.toChronoUnit()));
     }
 }
